75806cf399ea1e66bbb49477b7f0a6cab46ba560
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-03-13 14:28:14 -0400

    callbacks implemented

--- async.playground/Contents.swift
+++ async.playground/Contents.swift
@@ -3,6 +3,21 @@ import UIKit
 // Dispatch Queue
 
 // callbacks
+enum Result<Value> {
+    case value(Value)
+    case error(Error)
+}
+
+typealias Handler = (Result<Value> -> Void)
+
+func callback(urlString: String, callback: @escaping Handler) {
+    URLSession.shared.dataTask(with: "https://google.com") { (data, response, error) in
+        if let error = error {
+            
+        }
+    }
+    
+}
 
 // promises
 
